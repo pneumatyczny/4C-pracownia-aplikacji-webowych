34a68aef4295cffdf19a501d71ab0c9ac09ded23
pneumatyczny <furmanczykwladek@gmail.com>
2025-10-02 10:18:38 +0200

    Add files via upload
    Bazując na zadaniu z poprzedniej lekcji wykonaj następujące zadania.
    - Dodaj ścieżkę /get_params
    - Dla ścieżki /get_params wyświetl w konsoli Node.js parametry przekazane metodą GET.
    - Parametry GET skonwertuj do tablicy i zapisz w pliku params_{timestamp}.json, gdzie timestamp to timestamp przyjęcia danych. Format danych to JSON.
    - Zwróć ze ścieżki dane JSON o treści {'ok': 'ok'}
    
    Zrealizowano. Żeby uruchomić trzeba pobrać biblioteki.

---
+++ 2025.10.02/zadanie.js
@@ -0,0 +1,42 @@
+const http = require('http');
+const { readFile } = require('fs/promises');
+const url = require("node:url");
+let jsonFile = require('jsonfile');
+
+
+
+const srv = http.createServer(async(req, res) => {
+    const pathname = url.parse(req.url, true);
+
+
+    switch (true) {
+        case pathname.pathname === '/':
+            res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+            res.end('Strona główna')
+            break
+        case pathname.pathname === '/json':
+            const json = {Month: "December", Day: "1st"};
+            res.end(JSON.stringify(json));
+            break
+        case pathname.pathname === '/in':
+            res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+            res.end('<h2>Strona in</h2>')
+            break
+        case pathname.pathname === '/file':
+            const file = await readFile('plik.html', 'utf8')
+            res.end(file.toString())
+            break
+        case pathname.pathname === '/get_params':
+            const params = pathname.query
+            const time = new Date().getTime();
+            jsonFile.writeFile('params_'+time+'.json', params);
+
+            console.log(params);
+            console.log(time);
+            res.end(JSON.stringify({ok:"ok"}, null, 2));
+            break
+        default:
+            res.end('Error: Not Found!')
+    }
+
+}).listen(8080);
